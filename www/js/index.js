1aa94c2d5ae9c4d86e31e2a8f9c4d57e7e27abb0
consci-ap <ap.chokdee@gmail.com>
2018-02-27 20:51:40 +0700

    File path debug

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,7 @@
  * under the License.
  */
 var app = {
+    fileRoot: "",
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
@@ -28,6 +29,17 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+
+        console.log("Document URL: " + document.URL);
+        console.log('onDeviceReady: ' + device.platform + ' : ' + device.model);
+        var platform = device.platform.toLowerCase();
+        if(platform == "android") {
+          app.fileRoot = cordova.file.applicationDirectory + "www/";
+        } else if(platform == "ios") {
+          app.fileRoot = cordova.file.applicationDirectory + "www/";
+        }
+        console.log("fileRoot2: " + app.fileRoot);
+        playAudio(app.fileRoot + "audio/JigsawDone.mp3");
     },
 
     // Update DOM on a Received Event
@@ -44,3 +56,21 @@ var app = {
 };
 
 app.initialize();
+
+// Play audio
+//
+function playAudio(url) {
+    // Play the audio file at url
+    var my_media = new Media(url,
+        // success callback
+        function () {
+            console.log("playAudio():Audio Success");
+        },
+        // error callback
+        function (err) {
+            console.log("playAudio():Audio Error: " + err);
+        }
+    );
+    // Play audio
+    my_media.play();
+}
